1114015880242ba7a5665f0d90fda9b5fb23ed99
Jeff Tan <JeffTan.b@gmail.com>
2025-11-17 00:53:35 -0800

    separate debug examples

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ import manifest_io
 def main():
       # example receive
       puzzle_state = manifest_io.load_manifest_from_user()
-      debug.debug_print_formatted_loaded_manifest(puzzle_state)
 
+      debug.debug_print_formatted_loaded_manifest(puzzle_state)
       # debug.debug_print_raw_loaded_manifest(puzzle_state)
       debug.debug_print_weight_grid(puzzle_state)
 
