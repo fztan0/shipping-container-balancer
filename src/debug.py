881f70c7440a71dcb135f3cdd2aad1a9bdffbd2e
Bryce7832 <bryce.hojo@gmail.com>
2025-11-18 21:01:22 -0800

    Added basic goal handler

--- src/debug.py
+++ src/debug.py
@@ -1,4 +1,5 @@
 import puzzle_state
+import search_algorithm
 
 def debug_print_formatted_loaded_manifest(state: puzzle_state):
       for r in range(1, 9): # rows 1–8
@@ -32,7 +33,19 @@ def debug_print_raw_loaded_manifest(state: puzzle_state):
 
 def debug_print_weight_grid(state: puzzle_state):
       print("Weight Grid:")
-      for row in state.grid:
+      #reflect example from cs179 lecture slides
+      for row in reversed(state.grid):
             print([cell.weight if cell.exists else 'NAN' for cell in row])
-
       return
+
+def debug_print_totalWeight_eachSide(state: puzzle_state):
+      #reflect example from cs179 lecture slides
+      ph,sh = search_algorithm.getCurrentWeight(state)
+      print(f"Weight of PH: {ph}")
+      print(f"Weight of SH: {sh}")
+      p0,s0 = search_algorithm.getCurrentWeight(state)
+      isGoal = search_algorithm.isGoalState(state, p0, s0)
+      print(f"Is goal? : {isGoal}")
+      return
+
+
