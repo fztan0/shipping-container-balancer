78f6baa37aaab14ed76d737bdb3a69e82e2e2bf9
randall-nguyen <rnguy137@ucr.edu>
2025-11-25 16:57:30 -0800

    Cleaned up main

--- src/main.py
+++ src/main.py
@@ -5,18 +5,13 @@ import logger
 
 def main():
       # example receive
-      logger.initialize_logger()
-      manifest_io.load_manifest_from_user()
+      puzzle = manifest_io.load_manifest_from_user()
 
-      # debug.debug_print_formatted_loaded_manifest(puzzle)
-      # # debug.debug_print_raw_loaded_manifest(puzzle_state)
-      # debug.debug_print_weight_grid(puzzle)
-      #debug.debug_print_totalWeight_eachSide(puzzle)
-      list_moves = [("01", "04"), ("03", "03"), ("01", "03"), ("03", "04"), ("04", "01"), ("10", "02")]
-      logger.log_move_operation(list_moves)
-      logger.log_finish_operation()
-      logger.log_kill()
-      logger.write_logger_to_desktop()
+      debug.debug_print_formatted_loaded_manifest(puzzle)
+      # debug.debug_print_raw_loaded_manifest(puzzle_state)
+      debug.debug_print_weight_grid(puzzle)
+      debug.debug_print_totalWeight_eachSide(puzzle)
+
       # visualization.visualize_state(puzzle)
 
 
